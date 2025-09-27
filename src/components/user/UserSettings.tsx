bc3fde2ce249cf49d21a063066bc2904467b8c39
[dyad] <git@dyad.sh>
2025-09-27 13:22:04 -0400

    [dyad] Updated toggle buttons to be 25% lighter with darker borders - wrote 1 file(s)

--- src/components/user/UserSettings.tsx
+++ src/components/user/UserSettings.tsx
@@ -138,8 +138,8 @@ const UserSettings = () => {
         onClick={onToggle}
         className={`px-4 py-2 text-sm font-medium rounded-md transition-colors ${
           isOn 
-            ? 'bg-green-300 text-green-800 border border-green-400' 
-            : 'bg-red-300 text-red-800 border border-red-400'
+            ? 'bg-green-200 text-green-900 border border-green-700' 
+            : 'bg-red-200 text-red-900 border border-red-700'
         }`}
       >
         {isOn ? 'ON' : 'OFF'}
